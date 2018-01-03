b52d0fa77b68ef8843b480acd7ec3a3a741ba6ea
Tommy Ip <hkmp7tommy@gmail.com>
2018-01-03 17:49:02 +0000

    :tada: Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# PTLearn
+
+PTLearn is a deep learning library built on top of PyTorch. Inspired by TFLearn
+for TensorFlow, PTLearn provides a high-level API to PyTorch for quick and
+easy experimentation.
---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+setup(
+    name='ptlearn',
+    packages=['ptlearn'],
+    version='0.1.0',
+    description='High-level API for PyTorch',
+    author='Tommy Ip',
+    author_email='hkmp7tommy@gmail.com',
+    url='https://github.com/tommyip/ptlearn',
+    license='MIT',
+    install_requires=[
+        'numpy',
+    ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+        'Operating System :: POSIX :: Linux',
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence'
+    ],
+    keywords=[
+        'PTLearn',
+        'PyTorch',
+        'Deep Learning',
+        'Machine Learning',
+        'Neural Networks',
+        'AI'
+    ]
+)
