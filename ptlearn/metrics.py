b5f897d15cc8ab2b281558bef3c52a1b022178e8
Tommy Ip <hkmp7tommy@gmail.com>
2018-01-07 00:01:07 +0000

    metrics: Implement R^2 (coefficient of determination).

--- ptlearn/metrics.py
+++ ptlearn/metrics.py
@@ -1,15 +1,41 @@
+import numpy as np
+
 # Classification metrics
 
 
-def accuracy(predictions, targets):
+def Accuracy(pred, targets):
     """ The ratio of correct predictions to ground truth for categorical data.
 
     Args:
-        predictions (ndarray): One-hot encoded categorical prediction.
-        targets (ndarray): Ground truth class labels.
+        pred (`ndarray`): One-hot encoded categorical prediction.
+        targets (`ndarray`): Ground truth class labels.
 
     Returns:
-        float: Accuracy of model.
+        float: Accuracy of model ranging [0, 1], higher is better.
 
     """
-    return (predictions.argmax(axis=1) == targets).sum() / len(targets)
+    return (pred.argmax(axis=1) == targets).sum() / len(targets)
+
+
+# Regression metrics
+
+
+def R2(pred, targets):
+    """ R^2 (Coefficient of determination) is the measure of how well the
+    regression line approximates the real data samples.
+
+    Args:
+        pred (`ndarray`): Predicted target values.
+        targets (`ndarray`): Ground truth target values.
+
+    Returns:
+        `float` (maybe be negative). A R^2 of 1 indicates that the regression
+        line perfectly fits the data.
+
+    """
+
+    numerator = ((targets - pred) ** 2).sum(axis=0)
+    # TODO: Handle divide by 0.
+    denominator = ((targets - targets.mean(axis=0)) ** 2).sum(axis=0)
+
+    return np.mean(1 - (numerator / denominator))
